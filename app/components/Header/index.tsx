f3ff1af76a15cadf8e0dc065ce90f21d3ff7b2e5
thall <timothy.hallowell@gmail.com>
2023-10-23 19:56:55 -0400

    Added first shadcn/radix components

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import MainNav from "@/components/MainNav";
+
 interface HeaderProps {
   title: string;
 }
@@ -8,6 +10,7 @@ const Header: React.FC<HeaderProps> = ({ title }) => {
   return (
     <div className="w-full bg-slate-400">
       <span>{title}</span>
+      <MainNav />
     </div>
   );
 };
---
+++ components/MainNav/index.tsx
@@ -0,0 +1,32 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+
+interface MainNavProps {}
+
+const MainNav: React.FC<MainNavProps> = ({}) => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="outline">Open</Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Are you sure absolutely sure?</SheetTitle>
+          <SheetDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MainNav;
